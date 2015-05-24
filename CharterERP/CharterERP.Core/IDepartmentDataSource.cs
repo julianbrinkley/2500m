5450578e3d0f15689c0aaab209bd1e15557d815e
julianbrinkley <julianlbrinkley@gmail.com>
2015-05-23 20:21:12 -0500

    Created initial projects (Charter.Web and Charter.Core)

---
+++ CharterERP/CharterERP.Core/IDepartmentDataSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CharterERP.Core
+{
+   public  class IDepartmentDataSource
+    {
+
+       IQueryable<Employee> Employee { get; }
+       IQueryable<Department> Departments { get; }
+
+
+    }
+}
---
+++ CharterERP/CharterERP.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CharterERP.Web.MvcApplication" Language="C#" %>
